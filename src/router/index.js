2c730ac21a20c92d59ccf92d93612b21faf29404
billos-e <bill.ahognonvi@epitech.eu>
2025-09-16 12:48:49 +0100

    ADD: routes config

--- src/router/index.js
+++ src/router/index.js
@@ -1,12 +1,14 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import PostsView from '../views/PostsView.vue'
+import DetailView from '../views/DetailView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
-      name: 'home',
+      path: '/home',
+      name: 'init',
       component: HomeView,
     },
     {
@@ -17,6 +19,17 @@ const router = createRouter({
       // which is lazy-loaded when the route is visited.
       component: () => import('../views/AboutView.vue'),
     },
+    // project views
+    {
+      path: '/',
+      name: 'home',
+      component: PostsView,
+    },
+    {
+      path : '/note/:id' ,
+      name : 'note',
+      component : DetailView,
+    }
   ],
 })
 
