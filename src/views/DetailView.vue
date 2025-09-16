ff81ed645b3775925aee4a2976f67bacda105cf1
billos-e <bill.ahognonvi@epitech.eu>
2025-09-16 14:31:34 +0100

    ADD: creating files and views

---
+++ src/views/DetailView.vue
@@ -0,0 +1,17 @@
+<script setup>
+import HeaderItem from '../components/HeaderItem.vue'
+import FooterItem from '../components/FooterItem.vue'
+
+import { useRoute } from 'vue-router';
+const route = useRoute()
+// console.log(route.params)
+const id = route.params.id;
+</script>
+
+<template>
+  <HeaderItem />
+  <main>
+    <p>Detail page of {{ id }}</p>
+  </main>
+  <FooterItem />
+</template>
---
+++ src/views/PostsView.vue
@@ -0,0 +1,12 @@
+<script setup>
+import HeaderItem from '../components/HeaderItem.vue'
+import FooterItem from '../components/FooterItem.vue'
+</script>
+
+<template>
+  <HeaderItem />
+  <div>
+    <p>The post</p>
+  </div>
+  <FooterItem />
+</template>
