625e4efeb6018926f7ac3788533be47d5a141b4c
billos-e <bill.ahognonvi@epitech.eu>
2025-09-17 07:01:17 +0100

    ADD: adding blank methods ans links

--- src/views/PostsView.vue
+++ src/views/PostsView.vue
@@ -1,55 +1,44 @@
 <script setup>
 import HeaderItem from '../components/HeaderItem.vue'
 import FooterItem from '../components/FooterItem.vue'
+import {RouterLink} from 'vue-router'
+import { ref } from 'vue';
+
+let posts = ref([]);
+
+(async ()=> {
+  const respo = await fetch('https://post-it.epi-bluelock.bj/notes/')
+  const result= await respo.json()
+  posts.value = result['notes']
+  // console.log(posts);
+  
+})()
+console.log(posts);
+
+
+const addPost = () => {};
+const deleteAll = () => {};
+const deletePost = (id) => {};
+const editPost = (id) => {};
 </script>
 
 <template>
   <HeaderItem />
   <div id="edit-line">
-    <button>Coucher une idée</button>
-    <button>Remettre à blanc</button>
+    <button @click="addPost">Coucher une idée</button>
+    <button @click="deleteAll">Remettre à blanc</button>
   </div>
-  <div id="grid" class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
-    <div class="post">
-      <div>
-        <h3>Titre</h3>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti sint ut recusandae sit omnis similique itaque ullam sed dolorum minima distinctio iusto quam reiciendis fugiat facere, quia nesciunt provident vel?
-        </p>
-      </div>
-    </div>
-    <div class="post">
-      <div>
-        <h3>Titre</h3>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti sint ut recusandae sit omnis similique itaque ullam sed dolorum minima distinctio iusto quam reiciendis fugiat facere, quia nesciunt provident vel?
-        </p>
-      </div>
-    </div>
-    <div class="post">
-      <div>
-        <h3>Titre</h3>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti sint ut recusandae sit omnis similique itaque ullam sed dolorum minima distinctio iusto quam reiciendis fugiat facere, quia nesciunt provident vel?
-        </p>
-      </div>
-    </div>
-    <div class="post">
-      <div>
-        <h3>Titre</h3>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti sint ut recusandae sit omnis similique itaque ullam sed dolorum minima distinctio iusto quam reiciendis fugiat facere, quia nesciunt provident vel?
-        </p>
-      </div>
-    </div>
-    <div class="post">
-      <div>
-        <h3>Titre</h3>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti sint ut recusandae sit omnis similique itaque ullam sed dolorum minima distinctio iusto quam reiciendis fugiat facere, quia nesciunt provident vel?
-        </p>
-      </div>
-    </div>
+  <div id="grid" class="border grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
+    <div v-for="post in posts" :key='post' class="post shadow-sm">
+        <RouterLink :to="{name: 'note', params: { id: post._id }}">
+          <h3>{{ post.title }}</h3>
+          <p>
+            {{ post.content[0] }}
+          </p>
+        </RouterLink>
+        <button @click="editPost(post._id)">E</button>
+        <button @click="deletePost(post._id)">X</button>
+      </div>  
   </div>
   <FooterItem />
 </template>
