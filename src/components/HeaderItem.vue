c97c4420411da02275a93eb4d5d104ec462cc066
billos-e <bill.ahognonvi@epitech.eu>
2025-09-20 14:19:45 +0100

    ADD: starting header

--- src/components/HeaderItem.vue
+++ src/components/HeaderItem.vue
@@ -1,12 +1,14 @@
-<script setup> 
+<script setup>
+import { RouterLink } from 'vue-router';
 </script>
 
 <template>
-    <div>
-        <h1>Header</h1>
+  <div class="py-6 mb-6 bg-black/70 backdrop-blur-sm shadow-sm border-b border-gray-200 sticky top-0 z-50">
+    <div class="text-white text-center">
+      <RouterLink to="/" class="text-2xl sm:text-3xl font-semibold " >
+        THE Post It
+      </RouterLink>
+      <p>Là où reposent vos idées</p>
     </div>
+  </div>
 </template>
-
-<style>
-
-</style>
