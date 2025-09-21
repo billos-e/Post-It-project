62177a64ca35030f8e5024f2d9f1d38f6949f21b
billos-e <bill.ahognonvi@epitech.eu>
2025-09-21 12:22:22 +0100

    ADD: styling

--- src/components/HeaderItem.vue
+++ src/components/HeaderItem.vue
@@ -3,9 +3,9 @@ import { RouterLink } from 'vue-router';
 </script>
 
 <template>
-  <div class="py-6 mb-6 bg-black/70 backdrop-blur-sm shadow-sm border-b border-gray-200 sticky top-0 z-50">
+  <div class="py-6 mb-6 bg-[#003747]/90 backdrop-blur-sm shadow-sm border-b border-gray-200 sticky top-0 z-50">
     <div class="text-white text-center">
-      <RouterLink to="/" class="text-2xl sm:text-3xl font-semibold " >
+      <RouterLink :to="{name:'home'}" class="text-2xl sm:text-3xl font-semibold " >
         THE Post It
       </RouterLink>
       <p>Là où reposent vos idées</p>
