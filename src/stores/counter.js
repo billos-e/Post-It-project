1c04974007387b4fc1c5058baf44cffb12a1541f
billos-e <bill.ahognonvi@epitech.eu>
2025-09-21 18:30:50 +0100

    UPDATE: refactoring for fetch

--- src/stores/counter.js
+++ src/stores/counter.js
@@ -1,6 +1,6 @@
 import { ref, computed } from 'vue'
 import { defineStore } from 'pinia'
-import axios from 'axios'
+// import axios from 'axios' Cas axios commentés
 
 export const useCounterStore = defineStore('counter', () => {
   const count = ref(0)
@@ -25,17 +25,38 @@ export const usePosts = defineStore('postIts', {
       try {
 
         this.message = ''
-        const respo = await axios.get(url)
-        this.posts = respo.data.notes
-        console.log('Chargement tous les posts');
 
-        if(this.posts.length == 0) this.message = 'Pas de notes jusque là'
+        // const respo = await axios.get(url)
 
-      } catch (err) {
-        if(err.response) {
+        const inter = await fetch(url, {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+          },
+        })
+
+        if (!inter.ok) {
+
+          const errorText = await inter.text();
+          throw new Error(`Erreur HTTP: ${inter.status} - ${errorText.error.message}`);
+        } else {
+
+          const respo = await inter.json()
+          // console.log(respo);
+          this.posts = respo.notes
+
+          console.log('Chargement tous les posts');
+          if(this.posts.length == 0) this.message = 'Pas de notes jusque là'
+        }
+
+      } catch (error) {
+        /* if(err.response) {
           this.message = err.response.data.error.message
         } else this.message = 'Erreur de connexion, verifiez le reseau'
 
+        console.log('Error: ' + this.message); */
+
+        this.message = 'Erreur de connexion ou du serveur: ' + error.message;
         console.log('Error: ' + this.message);
       }
 
@@ -43,17 +64,31 @@ export const usePosts = defineStore('postIts', {
     async addPost(payload) {
       try {
         this.message = ''
-        await axios.post(url, payload)
-        this.getAllPosts()
-        console.log('Ajout nouveau post');
-      }catch (err) {
+        // await axios.post(url, payload)
+
+        const inter = await fetch(url, {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(payload),
+        })
+
+        if (!inter.ok) {
+
+          const errorText = await inter.text();
+          throw new Error(`Erreur HTTP: ${inter.status} - ${errorText.error.message}`);
+        } else {
+
+          this.getAllPosts()
+          console.log('Ajout nouveau post');
+        }
+
+      }catch (error) {
         alert("Erreur lors de l'ajout")
-        console.log("Adding error:" + err);
+        console.log("Adding error:" + error);
 
-        if(err.response) {
-          this.message = err.response.data.error.message
-        } else this.message = 'Erreur de connexion, verifiez le reseau'
-
+        this.message = 'Erreur de connexion ou du serveur: ' + error.message;
         console.log('Error: ' + this.message);
 
       }
@@ -62,46 +97,91 @@ export const usePosts = defineStore('postIts', {
 
       try {
         this.message = ''
-        const respo = await axios.get(url + id)
-        this.selectedPost = respo.data;
+
+        // const respo = await axios.get(url + id)
+
+        const inter = await fetch(url + id, {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+          },
+        })
+
+        if (!inter.ok) {
+
+          const errorText = await inter.json();
+          throw new Error(`Erreur HTTP: ${inter.status} - ${errorText.error.message}`);
+        } else {
+
+          const respo = await inter.json()
+
+          this.selectedPost = respo;
+          // console.log(this.);
+        }
+
         console.log('Chargement post: ' + id);
+      } catch (error) {
+        console.log(error);
 
-
-      } catch (err) {
-
-        if(err.response) {
-          this.message = err.response.data.error.message
-        } else this.message = 'Erreur de connexion, verifiez le reseau'
-
+        this.message = 'Erreur de connexion ou du serveur: ' + error.message;
         console.log('Error: ' + this.message);
       }
     },
     async deletePost(id) {
       try{
         this.message = ''
-        await axios.delete(url + id)
-        console.log('Suppression post: ' + id);
-        alert(`Post : ${id} supprimé avec succès`)
-        this.getAllPosts()
-      }catch(err){
-        if(err.response) {
-          this.message = err.response.data.error.message
-        } else this.message = 'Erreur de connexion, verifiez le reseau'
+        // await axios.delete(url + id)
 
+        const inter = await fetch(url + id, {
+          method: "DELETE",
+          headers: {
+            "Content-Type": "application/json",
+          },
+        })
+
+        if (!inter.ok) {
+
+          const errorText = await inter.text();
+          throw new Error(`Erreur HTTP: ${inter.status} - ${errorText.error.message}`);
+        } else {
+
+          console.log('Suppression post: ' + id);
+          alert(`Post : ${id} supprimé avec succès`)
+          this.getAllPosts()
+        }
+
+      }catch(error){
+
+        this.message = 'Erreur de connexion ou du serveur: ' + error.message;
         console.log('Error: ' + this.message);
       }
     },
     async updatePost(id, updated) {
       try{
         this.message = ''
-        await axios.put(url + id, updated)
-        console.log('Modification post: ' + id);
-        this.getPost(id)
-      }catch(err){
-        if(err.response) {
-          this.message = err.response.data.error.message
-        } else this.message = 'Erreur de connexion, verifiez le reseau'
+        // await axios.put(url + id, updated)
 
+        const inter = await fetch(url + id, {
+          method: "PUT",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(updated),
+        })
+
+        if (!inter.ok) {
+
+          const errorText = await inter.text();
+          throw new Error(`Erreur HTTP: ${inter.status} - ${errorText.error.message}`);
+        } else {
+
+          console.log('Modification post: ' + id);
+          this.getPost(id)
+        }
+
+      }catch(error){
+
+        this.message = 'Erreur de connexion ou du serveur: ' + error.message;
         console.log('Error: ' + this.message);
         alert('Error: ' + this.message)
       }
--- src/views/PostsView.vue
+++ src/views/PostsView.vue
@@ -38,6 +38,7 @@ onUnmounted(() => {
   clearInterval(rocketGetter)
 })
 // store.getAllPosts();
+store.selectedPost = null;
 </script>
 
 <template >
